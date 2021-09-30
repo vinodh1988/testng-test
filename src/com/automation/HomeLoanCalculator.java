a167fda6468877491dce0e99daeb08826a2c84b1
vinodhkc@vinodhconnects.onmicrosoft.com <vinodhkc@vinodhconnects.onmicrosoft.com>
2021-09-30 17:20:35 +0530

    emi-full-flow

--- src/com/automation/HomeLoanCalculator.java
+++ src/com/automation/HomeLoanCalculator.java
@@ -4,23 +4,53 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
+import com.info.EMIRecord;
+import com.info.EMIReport;
+
 public class HomeLoanCalculator {
-  public static void main(String n[]) {
-	 WebDriver wb= SiteLoader.load("https://emicalculator.net/");
+	
+	static WebDriver wb= SiteLoader.load("https://emicalculator.net/");
+	
+    public static void main(String n[]) {
+    	String items[][] = {
+    			{"100000000","7.5","10"},
+    			{"500000000","7.5","10"},
+    			{"200000000","8.5","16"},
+    			{"400000000","3.5","12"},
+    			{"300000000","7.5","10"},
+    			{"200000000","8.5","10"},
+    			{"100000000","8.5","10"},
+    			{"300000000","7.5","10"},
+    			{"120000000","7.5","12"},
+    			{"110000000","8.5","10"}
+    	};
+    	
+    	for(String[] x:items) {
+    		doActivty(x);
+    	}
+    	EMIReport.report();
+    }	
+    
+    public static void doActivty(String[] input) {
+	
 	 WebElement loanamount= wb.findElement(By.id("loanamount"));
 	 WebElement loaninterest= wb.findElement(By.id("loaninterest"));
 	 WebElement loanterm= wb.findElement(By.id("loanterm"));
 	 WebElement button=wb.findElement(By.xpath("//*[@id=\"emicalculatorinnerform\"]/div[1]/div/div/div"));
 	 loanamount.clear();
-	 loanamount.sendKeys("\b10000000");
+	 loanamount.sendKeys("\b"+input[0]);
 	 loaninterest.clear();
-     loaninterest.sendKeys("\b7.5");
+     loaninterest.sendKeys("\b"+input[1]);
      loanterm.clear();
-     loanterm.sendKeys("\b10");
+     loanterm.sendKeys("\b"+input[2]);
      button.click();
-
-
-     System.out.println("EMI Amount "+wb.findElement(By.xpath("//*[@id=\"emiamount\"]/p/span")).getText());
-     System.out.println("EMI Total interest "+wb.findElement(By.xpath("//*[@id=\"emitotalinterest\"]/p/span")).getText());
+    EMIRecord e=new EMIRecord();
+    e.setAmount(input[0]);
+    e.setInterest(input[1]);
+    e.setTerm(input[2]);
+    e.setEmiamount(wb.findElement(By.xpath("//*[@id=\"emiamount\"]/p/span")).getText());
+    e.setTotalinterest(wb.findElement(By.xpath("//*[@id=\"emitotalinterest\"]/p/span")).getText());
+    e.setTotalamount(wb.findElement(By.xpath("//*[@id=\"emitotalamount\"]/p/span")).getText());
+    EMIReport.addRecord(e);
   }
 }
--- src/com/info/EMIReport.java
+++ src/com/info/EMIReport.java
@@ -1,6 +1,26 @@
 package com.info;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class EMIReport {
-	//public static void List<EMIRecord>
+	public static 	List<EMIRecord> elist=new ArrayList<EMIRecord>();
+	
+	public static void addRecord(EMIRecord e) {
+	    elist.add(e);
+	}
+	
+	public static void report() {
+		System.out.println("Amount       Interest       Tenure       EMIAmount    TotalInterest     Total Amount");
+		System.out.println("-----------------------------------------------------------------------------------------------");
+	
+		for(EMIRecord record:elist) {
+			System.out.println(record.getAmount()+"     "+record.getInterest()+
+					"    "+record.getTerm()+"    "+record.getEmiamount()+"    "+record.getTotalinterest()
+					+"    "+record.getTotalamount());
+
+			
+		}
+	}
 
 }
