2e8314ba123582c3ed60ba9af5a6f9fadfeab841
vinodhkc@vinodhconnects.onmicrosoft.com <vinodhkc@vinodhconnects.onmicrosoft.com>
2021-09-30 15:08:11 +0530

    emi-commit-start

---
+++ src/com/automation/HomeLoanCalculator.java
@@ -0,0 +1,7 @@
+package com.automation;
+
+public class HomeLoanCalculator {
+  public static void main(String n[]) {
+	  SiteLoader.load("https://emicalculator.net/");
+  }
+}
---
+++ src/com/automation/SiteLoader.java
@@ -0,0 +1,12 @@
+package com.automation;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class SiteLoader {
+   public static void load(String url) {
+	   System.setProperty("webdriver.chrome.driver", "e:\\chromedriver.exe");
+		  WebDriver wb=new ChromeDriver();
+		  wb.get(url);
+   }
+}
