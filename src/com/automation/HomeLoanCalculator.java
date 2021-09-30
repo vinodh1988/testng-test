c8b80c0c679910df645896b3616383b389767133
vinodhkc@vinodhconnects.onmicrosoft.com <vinodhkc@vinodhconnects.onmicrosoft.com>
2021-09-30 15:29:47 +0530

    loan-calculator

--- src/com/automation/HomeLoanCalculator.java
+++ src/com/automation/HomeLoanCalculator.java
@@ -1,7 +1,22 @@
 package com.automation;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
 public class HomeLoanCalculator {
   public static void main(String n[]) {
-	  SiteLoader.load("https://emicalculator.net/");
+	 WebDriver wb= SiteLoader.load("https://emicalculator.net/");
+	 WebElement loanamount= wb.findElement(By.id("loanamount"));
+	 WebElement loaninterest= wb.findElement(By.id("loaninterest"));
+	 WebElement loanterm= wb.findElement(By.id("loanterm"));
+	 loanamount.clear();
+	 loanamount.sendKeys("\b10000000");
+	 loaninterest.clear();
+     loaninterest.sendKeys("\b7.5");
+     loanterm.clear();
+     loanterm.sendKeys("\b10");
+     
+
   }
 }
