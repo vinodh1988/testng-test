fa3d6cc5e9020795adcc0abdd02d2210ca9ade8a
vinodhkc@vinodhconnects.onmicrosoft.com <vinodhkc@vinodhconnects.onmicrosoft.com>
2021-10-04 14:38:53 +0530

    index-index

---
+++ index.js
@@ -0,0 +1,11 @@
+function showtest(){
+    setTimeout(()=>{
+        document.getElementById("container").innerHTML="<div id='message'>INDIA</div>";
+    },5000);
+}
+
+function showtest2(){
+  
+        document.getElementById("container2").innerHTML="<div id='message2'>ASIA</div>";
+ 
+}
