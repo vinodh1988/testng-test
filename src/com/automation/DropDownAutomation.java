ea521aa5a174327b7eeccb9923049bfcf12769cd
vinodhkc@vinodhconnects.onmicrosoft.com <vinodhkc@vinodhconnects.onmicrosoft.com>
2021-10-04 15:03:37 +0530

    select-complet

--- src/com/automation/DropDownAutomation.java
+++ src/com/automation/DropDownAutomation.java
@@ -2,17 +2,22 @@ package com.automation;
 
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
+ import static com.page.PageModel.*;
 
 import com.page.PageModel;
 
 public class DropDownAutomation {
     public static void main(String n[]) {
     	WebDriver wd=SiteLoader.load("http://www.google.co.in");
-    	PageModel.search(wd).sendKeys("Area Conversion");
+    	search(wd).sendKeys("Area Conversion");
     	//PageModel.button(wd).click();
     	JavascriptExecutor js=(JavascriptExecutor)wd;
-    	js.executeScript("arguments[0].click()",PageModel.button(wd));
+    	js.executeScript("arguments[0].click()",button(wd));
     	
-    	PageModel.select1(wd).selectByIndex(6);
+    	select1(wd).selectByIndex(6);
+    	select2(wd).selectByIndex(8);
+    	select3(wd).selectByIndex(1);
+    	
+        input(wd).sendKeys("\b1000");
     }
 }
--- src/com/page/PageModel.java
+++ src/com/page/PageModel.java
@@ -9,16 +9,30 @@ public class PageModel {
   
   private   static By searchbox = By.name("q");
   private static By searchButton = By.name("btnK");
+  private static By inputBox = By.xpath("//*[@jsname=\"axsL6b\"]");
+  
   private static By selectItem1 = By.xpath("//*[@jsname=\"MVliGc\"]");
+  private static By selectItem2 = By.xpath("//*[@jsname=\"De9Fne\"]");
+  private static By selectItem3 = By.xpath("//*[@jsname=\"iNUlwe\"]");
+  
   public static  WebElement search(WebDriver wb) {
 	  return wb.findElement(searchbox);
   }
   
+  public static  WebElement input(WebDriver wb) {
+	  return wb.findElement(inputBox);
+  }
   public static  WebElement button(WebDriver wb) {
 	  return wb.findElement(searchButton);
   }
   
   public static  Select select1(WebDriver wb) {
 	  return new Select(wb.findElement(selectItem1));
+  }
+  public static  Select select2(WebDriver wb) {
+	  return new Select(wb.findElement(selectItem2));
+  }
+  public static  Select select3(WebDriver wb) {
+	  return new Select(wb.findElement(selectItem3));
   }
 }
