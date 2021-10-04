a7755ec1f43d6de62928f83618757a8dae2dd086
vinodhkc@vinodhconnects.onmicrosoft.com <vinodhkc@vinodhconnects.onmicrosoft.com>
2021-10-04 14:49:33 +0530

    select-item

--- src/com/automation/DropDownAutomation.java
+++ src/com/automation/DropDownAutomation.java
@@ -8,9 +8,11 @@ import com.page.PageModel;
 public class DropDownAutomation {
     public static void main(String n[]) {
     	WebDriver wd=SiteLoader.load("http://www.google.co.in");
-    	PageModel.search(wd).sendKeys("Length Conversion");
+    	PageModel.search(wd).sendKeys("Area Conversion");
     	//PageModel.button(wd).click();
     	JavascriptExecutor js=(JavascriptExecutor)wd;
     	js.executeScript("arguments[0].click()",PageModel.button(wd));
+    	
+    	PageModel.select1(wd).selectByIndex(6);
     }
 }
--- src/com/page/PageModel.java
+++ src/com/page/PageModel.java
@@ -3,17 +3,22 @@ package com.page;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
 
 public class PageModel {
   
   private   static By searchbox = By.name("q");
   private static By searchButton = By.name("btnK");
-  
+  private static By selectItem1 = By.xpath("//*[@jsname=\"MVliGc\"]");
   public static  WebElement search(WebDriver wb) {
 	  return wb.findElement(searchbox);
   }
   
   public static  WebElement button(WebDriver wb) {
 	  return wb.findElement(searchButton);
+  }
+  
+  public static  Select select1(WebDriver wb) {
+	  return new Select(wb.findElement(selectItem1));
   }
 }
