5f3b4d1c0be1f10d75986c73402200149a09192a
vinodhkc@vinodhconnects.onmicrosoft.com <vinodhkc@vinodhconnects.onmicrosoft.com>
2021-10-04 16:03:53 +0530

    one-go-4-tests

--- src/com/automation/DropDownAutomation.java
+++ src/com/automation/DropDownAutomation.java
@@ -1,18 +1,29 @@
 package com.automation;
 
+import static com.page.PageModel.button;
+import static com.page.PageModel.input;
+import static com.page.PageModel.output;
+import static com.page.PageModel.search;
+import static com.page.PageModel.select1;
+import static com.page.PageModel.select2;
+import static com.page.PageModel.select3;
+
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
- import static com.page.PageModel.*;
-
-import com.page.PageModel;
 
 public class DropDownAutomation {
+	static WebDriver wd;
+	
+	static {
+		 wd=SiteLoader.load("http://www.google.co.in");
+			search(wd).sendKeys("Area Conversion");
+	    	//PageModel.button(wd).click();
+	    	JavascriptExecutor js=(JavascriptExecutor)wd;
+	    	js.executeScript("arguments[0].click()",button(wd));
+	}
     public static String execute(int type,int unit1,int unit2,String value) {
-    	WebDriver wd=SiteLoader.load("http://www.google.co.in");
-    	search(wd).sendKeys("Area Conversion");
-    	//PageModel.button(wd).click();
-    	JavascriptExecutor js=(JavascriptExecutor)wd;
-    	js.executeScript("arguments[0].click()",button(wd));
+    	
+    
     	
     	select1(wd).selectByIndex(type);
     	select2(wd).selectByIndex(unit1);
